20e76ba467b46edf9e2a99da54e571f3955242a9
itadmin <itadmin@computer.hsd1.wa.comcast.net>
2020-02-09 20:47:12 -0800

    start2

--- config/connection.js
+++ config/connection.js
@@ -1,34 +1,34 @@
-// // Set up MySQL connection.
-// var mysql = require("mysql");
-// var connection;
+// Set up MySQL connection.
+var mysql = require("mysql");
+var connection;
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "@Lph@20!!",
-//     database: "burgers_db"
-//   });
-// };
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "@Lph@20!!",
+    database: "burgers_db"
+  });
+};
 
-// // var connection = mysql.createConnection({
-// //   host: "gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-// //   port: 3306,
-// //   user: "f4eacne93uu8ugak",
-// //   password: "tmu7jo3w2q3uasvq",
-// //   database: "xrxk1fm1xb0io57b"
-// // });
-
-// // Make connection.
-// connection.connect(function (err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
+// var connection = mysql.createConnection({
+//   host: "gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   port: 3306,
+//   user: "f4eacne93uu8ugak",
+//   password: "tmu7jo3w2q3uasvq",
+//   database: "xrxk1fm1xb0io57b"
 // });
 
-// // Export connection for our ORM to use.
-// module.exports = connection;
+// Make connection.
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
